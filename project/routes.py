fee59172cf8437977de3a62ee373e890f09b296b
Conor Maguire <conormag94@gmail.com>
2018-01-23 19:52:13 +0000

    Add webhook endpoint to handle and verify github webhooks.

--- project/routes.py
+++ project/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, request, jsonify, current_app
+
+from project.utils import verify_signature
 
 wiki = Blueprint('wiki', __name__)
 
@@ -15,3 +17,16 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', notes=notes)
+
+@wiki.route('/webhook', methods=['POST'])
+def webhook_event():
+    """
+    Handles Github POST webhooks, authenticated with secret keys
+    """
+    received_signature = request.headers['X-Hub-Signature'] or None
+     
+    if received_signature and verify_signature(received_signature, request.data):
+        request_params = request.get_json()
+        return jsonify('Yep, got it'), 200
+    else:
+        return jsonify('Invalid security token'), 401
---
+++ project/utils.py
@@ -0,0 +1,15 @@
+from flask import current_app
+
+import hashlib
+import hmac
+
+def verify_signature(received_signature, payload_body):
+    """
+    Hashes the request payload using the secret and compares it with the
+    hash signature received by the Github webhook.
+    """
+    key = current_app.config['SECRET_KEY'].encode()
+    hasher = hmac.new(key, payload_body, hashlib.sha1)
+    
+    hashed_signature = 'sha1=' + hasher.hexdigest()
+    return hmac.compare_digest(hashed_signature, received_signature)
