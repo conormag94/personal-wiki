163cce20e06d448b42a7c06992210e88e763d394
Conor Maguire <conormag94@gmail.com>
2018-01-23 20:28:40 +0000

    Add config option to turn off webhook verification for debugging. Also cleaned up webhook route function by adding another util function.

--- project/routes.py
+++ project/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, jsonify, current_app
 
-from project.utils import verify_signature
+from project.utils import request_is_authorized
 
 wiki = Blueprint('wiki', __name__)
 
@@ -23,9 +23,7 @@ def webhook_event():
     """
     Handles Github POST webhooks, authenticated with secret keys
     """
-    received_signature = request.headers['X-Hub-Signature'] or None
-     
-    if received_signature and verify_signature(received_signature, request.data):
+    if request_is_authorized(request):
         request_params = request.get_json()
         return jsonify('Yep, got it'), 200
     else:
--- project/utils.py
+++ project/utils.py
@@ -13,3 +13,20 @@ def verify_signature(received_signature, payload_body):
     
     hashed_signature = 'sha1=' + hasher.hexdigest()
     return hmac.compare_digest(hashed_signature, received_signature)
+
+def request_is_authorized(request):
+    """
+    A webhook POST request is authorized under two conditions:
+
+    1. `VERIFY_WEBHOOKS` in config.py is False (debugging purposes).
+    2. `VERIFY_WEBHOOKS` is True and the signature header is verified, using
+       the `verify_signature()` function. 
+    """
+    if current_app.config['VERIFY_WEBHOOKS'] is False:
+        return True
+
+    received_signature = request.headers.get('X-Hub-Signature')
+    if received_signature and verify_signature(received_signature, request.data):
+        return True
+
+    return False
